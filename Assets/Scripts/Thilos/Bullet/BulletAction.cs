6a9610ebc71937b83af11bbe14b47837ece47afc
Thilos-Projects <50086979+Thilos-Projects@users.noreply.github.com>
2022-07-16 00:06:36 +0200

    BulletStart

---
+++ Assets/Scripts/Thilos/Bullet/BulletAction.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletAction : MonoBehaviour
+{
+    public Transform origine;
+    public Transform target;
+
+    public bulletScriptableObject prefab;
+    
+    float time;
+    SpriteRenderer renderer;
+
+    private void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
